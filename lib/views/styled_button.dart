e89be945461c6ae5f067bf311a24089b0ae1d328
smillwvy <171736343+smillwvy@users.noreply.github.com>
2025-12-12 13:27:13 +0000

    Add StyledButton widget for reusable button component

---
+++ lib/views/styled_button.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:sandwich_shop/views/app_styles.dart';
+
+class StyledButton extends StatelessWidget {
+  final VoidCallback? onPressed;
+  final IconData icon;
+  final String label;
+  final Color backgroundColor;
+
+  const StyledButton({
+    super.key,
+    required this.onPressed,
+    required this.icon,
+    required this.label,
+    required this.backgroundColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final ButtonStyle myButtonStyle = ElevatedButton.styleFrom(
+      backgroundColor: backgroundColor,
+      foregroundColor: Colors.white,
+      textStyle: normalText,
+    );
+
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: myButtonStyle,
+      child: Row(
+        children: [
+          Icon(icon),
+          const SizedBox(width: 8),
+          Text(label),
+        ],
+      ),
+    );
+  }
+}
