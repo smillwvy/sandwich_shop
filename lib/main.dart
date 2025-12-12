d392eeeee76fac398330e22c8ee60087cbb6b862
smillwvy <171736343+smillwvy@users.noreply.github.com>
2025-12-12 13:39:29 +0000

    Refactor main.dart to initialize font size and provide Cart model to the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:sandwich_shop/models/cart.dart';
 import 'package:sandwich_shop/views/order_screen.dart';
+import 'package:sandwich_shop/views/app_styles.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await AppStyles.loadFontSize();
   runApp(const App());
 }
 
@@ -10,9 +15,15 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Sandwich Shop App',
-      home: OrderScreen(maxQuantity: 5),
+    return ChangeNotifierProvider(
+      create: (BuildContext context) {
+        return Cart();
+      },
+      child: const MaterialApp(
+        title: 'Sandwich Shop App',
+        debugShowCheckedModeBanner: false,
+        home: OrderScreen(maxQuantity: 5),
+      ),
     );
   }
 }
